6b1b94c8a583ab6f23de0d458a511ea1cc530a36
BURHAN ALI <gamingcristy19@gmail.com>
2026-01-25 18:51:47 +0000

    Add comment to ImageCanvasComponent
    Added a comment above the ImageCanvasComponent function in ImageCanvas.tsx for clarification.

--- src/components/media/ImageSegmentEditor/components/ImageCanvas.tsx
+++ src/components/media/ImageSegmentEditor/components/ImageCanvas.tsx
@@ -29,7 +29,7 @@ interface ImageCanvasProps {
   onUpdatePan: (x: number, y: number) => void;
   onStopPanning: () => void;
 }
-
+//this snaps here
 function ImageCanvasComponent({
   imageSrc,
   regions,
